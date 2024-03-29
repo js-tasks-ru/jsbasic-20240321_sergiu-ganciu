2ba796240744ad4edb97796593169951ecc39c03
Serghey Ganciu <segheiganciu33@gmail.com>
2024-03-30 00:06:23 +0200

    Task3.3: Create calculator object

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,46 @@
 let calculator = {
-  // ваш код
+  /**
+   * Insert values into object
+   * @param {} val1
+   * @param {} val2
+   * @returns {}
+   */
+  read(val1, val2) {
+    this.val1 = val1;
+    this.val2 = val2;
+  },
+
+  /**
+   * Calculate object's numeric properties
+   * @returns {Number}
+   */
+  sum() {
+    let sum = 0;
+
+    for (let key in this) {
+      if (Number.isFinite(this[key])) {
+        sum += this[key];
+      }
+    }
+    
+    return sum;
+  },
+
+  /**
+   * Multiply object's numeric properties
+   * @returns {Number}
+   */
+  mul() {
+    let mul = 1;
+
+    for (let key in this) {
+      if (Number.isFinite(this[key])) {
+        mul *= this[key];
+      }
+    }
+
+    return mul;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
