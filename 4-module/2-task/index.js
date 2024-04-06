9af1d494df4fce39344628c4214564cfa8b4399a
Serghey Ganciu <segheiganciu33@gmail.com>
2024-04-06 11:30:47 +0300

    Task5.2: Add background color to the diagonal cell

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,22 @@
+/**
+ * Add background color to the diagonal cell
+ * @param {object} table
+ * @returns {}
+ */
 function makeDiagonalRed(table) {
-  // ваш код...
-}
+  let countRow = 0;
+  
+  for (const row of table.rows){
+    let countCell = 0;
+    
+    for (const cell of row.cells){
+      if (countRow == countCell){
+        cell.style.backgroundColor = "red";
+      }
+      
+      countCell++;
+    }
+    
+    countRow++;
+  }
+}    
