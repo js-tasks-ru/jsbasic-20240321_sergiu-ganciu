e396922232cee43a2624ceeaa6532ef67fb9fb15
Serghey Ganciu <segheiganciu33@gmail.com>
2024-04-10 21:41:17 +0300

    Task6.2: Hide div on button click

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
+/**
+ * Hide div on button click
+ * @returns {}
+ */
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('.toggle-text-button');
+  button.addEventListener('click', () => {
+    const div = document.querySelector('#text');
+    div.hasAttribute('hidden') ? div.removeAttribute('hidden') : div.setAttribute('hidden', '');
+  });
 }
