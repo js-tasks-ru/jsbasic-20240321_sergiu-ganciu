2647ca052bf2392d3e36562e196f3d8798430d51
Serghey Ganciu <segheiganciu33@gmail.com>
2024-04-24 10:57:42 +0300

    Task7.1: Create table with rows removing

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,59 @@
  *
  */
 export default class UserTable {
+  elem = null;
+  #rows = []
+
   constructor(rows) {
+    this.#rows = rows || this.#rows;
+    this.elem = this.#render();
+  }
+
+  #template() {
+    return `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        ${
+          this.#rows.map(value => 
+            `<tr>
+              <td>${value.name}</td>
+              <td>${value.age}</td>
+              <td>${value.salary}</td>
+              <td>${value.city}</td>
+              <td><button class="js-remove-button">X</button></td>
+            </tr>`
+          ).join('\n')
+        }
+      </tbody>
+    `
+  }
+
+  #onRemoveClick = (event) => {
+    const target = event.target;
+    const row = target.closest("tr");
+
+    if (!target.classList.contains("js-remove-button") || !row) {
+      return;
+    }
+
+    row.remove();
+  }
+  
+  #render() {
+    this.elem = document.createElement("table");
+    
+    this.elem.innerHTML = this.#template();
+
+    this.elem.addEventListener('click', this.#onRemoveClick)
+
+    return this.elem;
   }
 }
